c314936196113cb5e4e9247ce24397269a1efa17
Tnpob1 <titleetinnapob@gmail.com>
2023-10-06 19:41:39 +0700

    pinia

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/Nav-bar.vue'
+import Cards from './components/Cafe-card.vue'
+</script>
+
+<template>
+  <div class="bg-gray-100">
+    <NavBar />
+    <Cards />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
